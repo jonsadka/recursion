73b2d6784f263bc753dc17651b9d687afb3f3d4a
Jon Sadka <jonsadka@gmail.com>
2014-06-27 10:32:43 -0700

    updated stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,28 +4,35 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   if (obj === undefined){
-    return 'You need to input something... silly';
+    return;
   }
 
   var string = '';
 
   if (Object.prototype.toString.call(obj) === '[object Object]'){
     string = '{';
+    if (Object.getOwnPropertyNames(obj).length === 0){
+      string = string + '{';
+    }
     for (key in obj){
       string = string + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
     }
     return string.slice(0,-1) + '}';
-    console.log('Object! Woo!');
   } else if (Object.prototype.toString.call(obj) === '[object Array]'){
     string = '[';
+    if (obj.length === 0){
+      string = '[]';
+    }
     for (var i=0; i<obj.length; i++){
       string = string + stringifyJSON(obj[i]) + ',';
     }
     return string.slice(0,-1) + ']';
+  } else if (obj === null){
+    return String(obj);
   } else if (typeof obj === 'string'){
     return '\"' + obj + '\"';
   } else {
-    return obj;
+    return obj.toString();
   }
 
   return stringifyJSON(obj);
