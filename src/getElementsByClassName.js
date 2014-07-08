fd19c5a4839c65366db5b5116f938f67c72c057a
Jon Sadka <jonsadka@gmail.com>
2014-07-07 22:15:46 -0700

    removed log notes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,17 +6,12 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
 	var result = [];
-	console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXNEWXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+	
  	var find = function(collection){
- 		//console.log('asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf')
- 		//console.log(collection);
- 		//console.log(collection.hasChildNodes());
  		
  		var checkClass = function(item){
 	 		if (item.classList){
 				for (var h=0; h<item.classList.length; h++){
-					//console.log(item.classList[h]);
-					//console.log("YAY!");
 					if (item.classList[h] === className){
 						result.push(item);
 					}
@@ -26,22 +21,13 @@ var getElementsByClassName = function(className){
 
  		checkClass(collection);
 
-		//console.log("Collection has child nodes: " + collection.hasChildNodes());
  		if (collection.hasChildNodes()){
- 			//console.log("There are " +collection.childNodes.length+ " child nodes");
  			var nodes = collection.childNodes;
  			for (var i=0; i<nodes.length; i++){
- 				
- 				//console.log('------Node-----');
- 				//console.log(nodes[i]);
- 				//console.log("Has "+nodes[i].childNodes.length+" child nodes: " + nodes[i].hasChildNodes());
- 				//console.log(nodes[i].className);	
 
  				if (nodes[i].hasChildNodes()){
- 					//console.log("___Run find() on these "+nodes[i].childNodes.length+" nodes___");
  					checkClass(nodes[i]);
  					for (var j=0; j<nodes[i].childNodes.length; j++){
- 						//console.log(nodes[i].childNodes[j]);
   					find(nodes[i].childNodes[j]);	 							
  					}
  				} else {
@@ -54,7 +40,5 @@ var getElementsByClassName = function(className){
 
  	find(document.body);
 
- 	console.log('_______________________RESULT___________________');
- 	console.log(result);
  	return result;
 };
