bf6cf37f6984ef8f29b814b04bf02230c382d5ac
Jon Sadka <jonsadka@gmail.com>
2014-07-07 22:13:21 -0700

    completed .getElementsByClassName and condensed code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,36 +8,44 @@ var getElementsByClassName = function(className){
 	var result = [];
 	console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXNEWXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
  	var find = function(collection){
- 		console.log(collection);
- 		console.log(collection.childNodes);
+ 		//console.log('asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf')
+ 		//console.log(collection);
+ 		//console.log(collection.hasChildNodes());
  		
- 		if (collection.className === className){
- 			result.push(collection);
- 		} 
+ 		var checkClass = function(item){
+	 		if (item.classList){
+				for (var h=0; h<item.classList.length; h++){
+					//console.log(item.classList[h]);
+					//console.log("YAY!");
+					if (item.classList[h] === className){
+						result.push(item);
+					}
+				}
+			}			
+ 		}
 
-		console.log("Collection has child nodes: " + collection.hasChildNodes());
+ 		checkClass(collection);
+
+		//console.log("Collection has child nodes: " + collection.hasChildNodes());
  		if (collection.hasChildNodes()){
- 			console.log("There are " +collection.childNodes.length+ " child nodes");
+ 			//console.log("There are " +collection.childNodes.length+ " child nodes");
  			var nodes = collection.childNodes;
  			for (var i=0; i<nodes.length; i++){
  				
- 				console.log('------Node-----');
- 				console.log(nodes[i]);
- 				console.log("Has "+nodes[i].childNodes.length+" child nodes: " + nodes[i].hasChildNodes());
+ 				//console.log('------Node-----');
+ 				//console.log(nodes[i]);
+ 				//console.log("Has "+nodes[i].childNodes.length+" child nodes: " + nodes[i].hasChildNodes());
+ 				//console.log(nodes[i].className);	
+
  				if (nodes[i].hasChildNodes()){
- 					console.log("___Run find() on these "+nodes[i].childNodes.length+" nodes___");
+ 					//console.log("___Run find() on these "+nodes[i].childNodes.length+" nodes___");
+ 					checkClass(nodes[i]);
  					for (var j=0; j<nodes[i].childNodes.length; j++){
  						//console.log(nodes[i].childNodes[j]);
   					find(nodes[i].childNodes[j]);	 							
  					}
  				} else {
- 					if (nodes[i].classList){
- 						for (var k=0; k<nodes[i].classList.length; k++){
- 							if (nodes[i].classList[k] === className){
- 								result.push(nodes[i]);
- 							}
- 						}
- 					}
+ 					checkClass(nodes[i]);
  				}
  			}
  		} 
