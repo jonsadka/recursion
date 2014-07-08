26dbe0fd3b6b070462672d6e2ce69ef0a3990e34
Jon Sadka <jonsadka@gmail.com>
2014-07-07 22:20:33 -0700

    converted tabs to spaces

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,40 +5,40 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-	var result = [];
-	
- 	var find = function(collection){
- 		
- 		var checkClass = function(item){
-	 		if (item.classList){
-				for (var h=0; h<item.classList.length; h++){
-					if (item.classList[h] === className){
-						result.push(item);
-					}
-				}
-			}			
- 		}
+  var result = [];
+  
+  var find = function(collection){
+    
+    var checkClass = function(item){
+      if (item.classList){
+        for (var h=0; h<item.classList.length; h++){
+          if (item.classList[h] === className){
+            result.push(item);
+          }
+        }
+      }     
+    }
 
- 		checkClass(collection);
+    checkClass(collection);
 
- 		if (collection.hasChildNodes()){
- 			var nodes = collection.childNodes;
- 			for (var i=0; i<nodes.length; i++){
+    if (collection.hasChildNodes()){
+      var nodes = collection.childNodes;
+      for (var i=0; i<nodes.length; i++){
 
- 				if (nodes[i].hasChildNodes()){
- 					checkClass(nodes[i]);
- 					for (var j=0; j<nodes[i].childNodes.length; j++){
-  					find(nodes[i].childNodes[j]);	 							
- 					}
- 				} else {
- 					checkClass(nodes[i]);
- 				}
- 			}
- 		} 
+        if (nodes[i].hasChildNodes()){
+          checkClass(nodes[i]);
+          for (var j=0; j<nodes[i].childNodes.length; j++){
+            find(nodes[i].childNodes[j]);               
+          }
+        } else {
+          checkClass(nodes[i]);
+        }
+      }
+    } 
 
-	 }
+   }
 
- 	find(document.body);
+  find(document.body);
 
- 	return result;
+  return result;
 };
